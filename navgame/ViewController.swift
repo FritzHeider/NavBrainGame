a051d3d2c6965c4a5fcf3d9a816641bc257fcec5
FritzHeider <fritz.heider@students.makeschool.com>
2019-12-08 03:21:27 -0800

    good start

--- navgame/ViewController.swift
+++ navgame/ViewController.swift
@@ -1,20 +1,21 @@
 //
-//  ViewController.swift
+//  GameViewController.swift
 //  navgame
 //
 //  Created by Fritz Heider on 12/8/19.
 //  Copyright © 2019 Make School. All rights reserved.
 //
 
-import UIKit
+import SwiftUI
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+struct GameViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-
-
 }
 
+struct GameViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        GameViewController()
+    }
+}
