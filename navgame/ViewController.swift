65aeee15ff3fc310e9263d65c8f321384a5a6afe
FritzHeider <fritz.heider@students.makeschool.com>
2019-12-08 03:01:25 -0800

    Initial Commit

---
+++ navgame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  navgame
+//
+//  Created by Fritz Heider on 12/8/19.
+//  Copyright © 2019 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
